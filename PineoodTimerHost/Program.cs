e7a65a1bb6bb2665da749104ca9d21cf623deb9a
Jay DeBoer <jaydeboer@gmail.com>
2018-02-18 19:13:59 -0600

    Initial commit

--- PineoodTimerHost/Program.cs
+++ PineoodTimerHost/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Timers;
 
 namespace PineoodTimerHost
 {
@@ -6,7 +11,74 @@ namespace PineoodTimerHost
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            hub = new HubConnectionBuilder()
+                .WithUrl("http://localhost:54856/statshub")
+                .Build();
+            hub.StartAsync().Wait();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                try
+                {
+                    SerialPort mySerialPort = new SerialPort(port);
+                    mySerialPort.BaudRate = 115200;
+                    mySerialPort.Parity = Parity.None;
+                    mySerialPort.StopBits = StopBits.One;
+                    mySerialPort.DataBits = 8;
+                    mySerialPort.Handshake = Handshake.None;
+                    mySerialPort.RtsEnable = false;
+                    mySerialPort.DataReceived += MySerialPort_DataReceived;
+                    mySerialPort.Open();
+                    Console.WriteLine($"Opened port {port}");
+                }
+                catch
+                {
+                    Console.WriteLine("No ports found");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static Stopwatch raceTimer = new Stopwatch();
+        private static Timer timeout = new Timer(10000);
+        private static List<char> finishedLanes = new List<char>();
+        private static HubConnection hub;
+
+        private static void MySerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string inData = sp.ReadExisting();
+            foreach (char data in inData)
+                ProcessChar(data);
+        }
+
+        static void ProcessChar(char data)
+        {
+            Console.WriteLine($"Received {data}.");
+            TimeSpan ts = raceTimer.Elapsed;
+            string time = ((double)ts.Seconds + (((double)ts.Milliseconds) / 1000)).ToString("N3");
+            switch (data)
+            {
+                case 's':
+                case 'S': // Race started
+                    raceTimer.Reset();
+                    raceTimer.Start();
+                    finishedLanes.Clear();
+                    timeout.Enabled = true;
+                    hub.InvokeAsync("StartRace");
+
+                    break;
+                case '1': // Lane 1 finished.
+                case '2': // Lane 2 finished
+                case '3': // Lane 3 finished
+                case '4': // Lane 4 finished
+                    finishedLanes.Add(data);
+                    hub.InvokeAsync("LaneFinished", data, finishedLanes.Count, time);
+                    Console.WriteLine($"Lane {data} finished with rank {finishedLanes.Count + 1} with an ET of {time} seconds");
+                    break;
+                default: break;
+            }
         }
     }
 }
---
+++ PinewoodTimerStatsBoard/StatsHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace PinewoodTimerStatsBoard
+{
+    public class StatsHub : Hub
+    {
+        public Task StartRace()
+        {
+            Console.WriteLine("Race Started");
+            return Clients.All.InvokeAsync("StartRace");
+        }
+        public Task LaneFinished(int lane, int place, string time)
+        {
+            Console.WriteLine($"Lane {lane} in place {place} with time {time}");
+            return Clients.All.InvokeAsync("LaneFinished", lane, place, time);
+        }
+    }
+}
