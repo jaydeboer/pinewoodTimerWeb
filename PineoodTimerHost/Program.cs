60b2276de67f591cb57bd499c09b40c7887a8cb0
Jay DeBoer <jay.deboer@digikey.com>
2018-02-19 21:04:55 -0600

    Added way to simulate multiple com ports

--- PineoodTimerHost/Program.cs
+++ PineoodTimerHost/Program.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Ports;
+using System.Threading.Tasks;
 using System.Timers;
+using System.Linq;
 
 namespace PineoodTimerHost
 {
@@ -11,15 +13,47 @@ namespace PineoodTimerHost
     {
         static void Main(string[] args)
         {
-            hub = new HubConnectionBuilder()
+            portWatcher.Elapsed += portWatcher_Elapsed;
+            signalrConnection = new HubConnectionBuilder()
                 .WithUrl("http://localhost:54856/statshub")
                 .Build();
-            hub.StartAsync().Wait();
+            signalrConnection.StartAsync().Wait();
 
+            portWatcher_Elapsed(null, null);
+            if (!knownPorts.Any())
+            {
+                knownPorts.Add("COM1");
+            }
+            selectedPort = knownPorts[0];
+
+            PrintInstructions();
+
+            char pressed;
+            do
+            {
+                pressed = Console.ReadKey(true).KeyChar;
+                if (pressed != 'e')
+                    ProcessChar(pressed, selectedPort);
+            } while (pressed != 'e');
+        }
+
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("The commands are:");
+            Console.WriteLine("=================");
+            Console.WriteLine("'s' - start new race");
+            Console.WriteLine("'1 - 4' - number of the lane to finish");
+            Console.WriteLine("'p' - create a new port");
+            Console.WriteLine("'t' - toggle through the ports");
+            Console.WriteLine("'e' - exit");
+        }
+
+        private static void portWatcher_Elapsed(object sender, ElapsedEventArgs e)
+        {
             try
             {
-                string[] ports = SerialPort.GetPortNames();
-                foreach (string port in ports)
+                var ports = SerialPort.GetPortNames().Where(portName => !knownPorts.Contains(portName));
+                foreach (var port in ports)
                 {
                     SerialPort mySerialPort = new SerialPort(port);
                     mySerialPort.BaudRate = 115200;
@@ -31,37 +65,33 @@ namespace PineoodTimerHost
                     mySerialPort.DataReceived += MySerialPort_DataReceived;
                     mySerialPort.Open();
                     Console.WriteLine($"Opened port {port}");
+                    knownPorts.Add(port);
                 }
             }
             catch
             {
-                Console.WriteLine("No ports found");
             }
-            char pressed;
-            do
-            {
-                pressed = Console.ReadKey().KeyChar;
-                if (pressed != 'e')
-                    ProcessChar(pressed);
-            }  while (pressed != 'e');
         }
 
         private static Stopwatch raceTimer = new Stopwatch();
         private static Timer timeout = new Timer(10000);
+        private static Timer portWatcher = new Timer(10000);
         private static HashSet<char> finishedLanes = new HashSet<char>();
-        private static HubConnection hub;
+        private static List<string> knownPorts = new List<string>();
+        private static HubConnection signalrConnection;
+        private static string selectedPort;
 
         private static void MySerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
             string inData = sp.ReadExisting();
             foreach (char data in inData)
-                ProcessChar(data);
+                ProcessChar(data, sp.PortName);
         }
 
-        static void ProcessChar(char data)
+        static void ProcessChar(char data, string portName)
         {
-            Console.WriteLine($"Received {data}.");
+            //Console.WriteLine($"Received {data}.");
             TimeSpan ts = raceTimer.Elapsed;
             string time = ((double)ts.Seconds + (((double)ts.Milliseconds) / 1000)).ToString("N3");
             switch (data)
@@ -72,7 +102,8 @@ namespace PineoodTimerHost
                     raceTimer.Start();
                     finishedLanes.Clear();
                     timeout.Enabled = true;
-                    hub.InvokeAsync("StartRace");
+                    Console.WriteLine();
+                    signalrConnection.InvokeAsync("StartRace");
 
                     break;
                 case '1': // Lane 1 finished.
@@ -80,8 +111,23 @@ namespace PineoodTimerHost
                 case '3': // Lane 3 finished
                 case '4': // Lane 4 finished
                     finishedLanes.Add(data);
-                    hub.InvokeAsync("LaneFinished", data, finishedLanes.Count, time);
-                    Console.WriteLine($"Lane {data} finished with rank {finishedLanes.Count + 1} with an ET of {time} seconds");
+                    signalrConnection.InvokeAsync("LaneFinished", data, finishedLanes.Count, time);
+                    //Console.WriteLine($"Lane {data} finished with rank {finishedLanes.Count + 1} with an ET of {time} seconds");
+                    break;
+                // the rest of these commands are for testing only
+                case 'p':
+                case 'P':
+                    knownPorts.Add($"COM{knownPorts.Count + 1}");
+                    Console.WriteLine($"Added port {knownPorts.Last()}");
+                    break;
+                case 't':
+                case 'T':
+                    var currentIndex = knownPorts.IndexOf(selectedPort);
+                    var newIndex = currentIndex+1;
+                    if (newIndex >= knownPorts.Count)
+                        newIndex = 0;
+                    selectedPort = knownPorts[newIndex];
+                    Console.WriteLine($"Selected port {selectedPort}");
                     break;
                 default: break;
             }
