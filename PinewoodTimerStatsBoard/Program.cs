a7e3d553b993589cd378e6667c0efcdeb2f7fe6c
Jay DeBoer <jay.deboer@digikey.com>
2018-02-20 18:59:48 -0600

    add QRCode for link to view

--- PinewoodTimerStatsBoard/Program.cs
+++ PinewoodTimerStatsBoard/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Linq;
+using System;
+using System.Net.NetworkInformation;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using ZXing;
 
 namespace PinewoodTimerStatsBoard
 {
@@ -7,6 +11,19 @@ namespace PinewoodTimerStatsBoard
     {
         public static void Main(string[] args)
         {
+            var myIp = GetLocalIPv4(NetworkInterfaceType.Ethernet);
+            if (string.IsNullOrWhiteSpace(myIp))
+                myIp = GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+            else if (string.IsNullOrWhiteSpace(myIp))
+                myIp = "127.0.0.1";
+            Console.WriteLine(myIp);
+
+            var w = new ZXing.BarcodeWriterSvg();
+            w.Format = BarcodeFormat.QR_CODE;
+            var image = w.Write($"http://{myIp}:54856");
+            
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(".","wwwroot", "images"));
+            System.IO.File.WriteAllText(System.IO.Path.Combine(".","wwwroot", "images", "barcode.svg"), image.Content);
             BuildWebHost(args).Run();
         }
 
@@ -15,5 +32,30 @@ namespace PinewoodTimerStatsBoard
                 .UseUrls("http://*:54856")
                 .UseStartup<Startup>()
                 .Build();
+        
+        internal static string GetLocalIPv4(NetworkInterfaceType type)
+        {
+            string output = string.Empty;
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties adapterProperties = item.GetIPProperties();
+
+                    if (adapterProperties.GatewayAddresses.FirstOrDefault() != null)
+                    {
+                        foreach (UnicastIPAddressInformation ip in adapterProperties.UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+                                output = ip.Address.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return output;
+        }
     }
 }
